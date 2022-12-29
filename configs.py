88ed17ff77a4417dbeed5fa7a8e159d3bec6ebcc
sahilgit55 <sahilgit55@gmail.com>
2022-12-30 02:25:20 +0530

    test

--- configs.py
+++ configs.py
@@ -8,19 +8,19 @@ import os
 
 
 class Config(object):
-	Session_String = os.environ.get("BOT_TOKEN", "5980925925:AAFNqaUEE6vpp4_k2ijoilJ5x870s7eIIx0")
-	API_ID = int(os.environ.get("API_ID", "8256203"))
-	API_HASH = os.environ.get("API_HASH", "45b3b561298ee72408e08a3c46d5f697")
+	Session_String = os.environ.get("Session_String", "")
+	API_ID = int(os.environ.get("API_ID", ""))
+	API_HASH = os.environ.get("API_HASH", "")
 	STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "NoNeed")
 	STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", "NoNeed")
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001628210353"))
+	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
 	DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
 	PRESET = os.environ.get("PRESET", "veryfast")
-	OWNER_ID = int(os.environ.get("OWNER_ID", "5968806660"))
+	OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 	CAPTION = "By @AHToolsBot"
 	BOT_USERNAME = "ANTON"
-	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Manoj123:Manoj123@cluster0.z0c2mif.mongodb.net/?retryWrites=true&w=majority")
+	DATABASE_URL = os.environ.get("DATABASE_URL", "")
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	ALLOW_UPLOAD_TO_STREAMTAPE = bool(os.environ.get("ALLOW_UPLOAD_TO_STREAMTAPE", False))
 	USAGE_WATERMARK_ADDER = """
